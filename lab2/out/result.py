122008de5bcad860b3e518bed6a23dae963beb1c
david.albuquerque <david.albuquerque@b2wdigital.com>
2021-11-30 00:23:01 -0300

    lab2: done

--- lab2/out/result.py
+++ lab2/out/result.py
@@ -22,58 +22,74 @@ for dim in (500,1000,2000):
     minSequentially.append(minSequentiallyValue)
     minConcurrently.append(minConcurrentlyValue)
     minFreeMemory.append(minFreeMemoryValue)
-print(minFreeMemory)
-print(minSequentially)
-print(minConcurrently)
+# print(minFreeMemory)
+# print(minSequentially)
+# print(minConcurrently)
 
 minSequentially.insert(0, "Sequencial")
-minSequentially.insert(4, "Sequencial")
-minSequentially.insert(8, "Sequencial")
 
 minConcurrently.insert(0, "Concorrente: 1 Thread")
 minConcurrently.insert(4, "Concorrente: 2 Thread")
 minConcurrently.insert(8, "Concorrente: 4 Thread")
 
 minFreeMemory.insert(0, "Liberar memória")
-minFreeMemory.insert(4, "Liberar memória")
-minFreeMemory.insert(8, "Liberar memória")
 
 outFinal = []
 
-out1 = minSequentially[:4]
-out2 = minSequentially[4:8]
-out3 = minSequentially[8:12]
+out = minSequentially[:1]
+out.append(minSequentially[1])
+out.append(minSequentially[4])
+out.append(minSequentially[7])
+outFinal.append(out)
 
-outFinal.append(out1)
-outFinal.append(out2)
-outFinal.append(out3)
+out = minConcurrently[0:1]
+out.append(minConcurrently[1])
+out.append(minConcurrently[5])
+out.append(minConcurrently[9])
+outFinal.append(out)
 
-out1 = minConcurrently[:4]
-out2 = minConcurrently[4:8]
-out3 = minConcurrently[8:12]
+out = ['Desempenho: 1 Thread']
+out.append(minSequentially[1] / minConcurrently[1])
+out.append(minSequentially[4] / minConcurrently[5])
+out.append(minSequentially[7] / minConcurrently[9])
+outFinal.append(out)
 
-outFinal.append(out1)
-outFinal.append(out2)
-outFinal.append(out3)
+out = minConcurrently[4:5]
+out.append(minConcurrently[2])
+out.append(minConcurrently[6])
+out.append(minConcurrently[10])
+outFinal.append(out)
 
-out1 = minFreeMemory[:4]
-out2 = minFreeMemory[4:8]
-out3 = minFreeMemory[8:12]
+out = ['Desempenho: 2 Threads']
+out.append(minSequentially[1] / minConcurrently[2])
+out.append(minSequentially[4] / minConcurrently[6])
+out.append(minSequentially[7] / minConcurrently[10])
+outFinal.append(out)
 
-outFinal.append(out1)
-outFinal.append(out2)
-outFinal.append(out3)
+out = minConcurrently[8:9]
+out.append(minConcurrently[3])
+out.append(minConcurrently[7])
+out.append(minConcurrently[11])
+outFinal.append(out)
 
-outFinal.append(out1)
-outFinal.append(out2)
-outFinal.append(out3)
+out = ['Desempenho: 4 Threads']
+out.append(minSequentially[1] / minConcurrently[3])
+out.append(minSequentially[4] / minConcurrently[7])
+out.append(minSequentially[7] / minConcurrently[11])
+outFinal.append(out)
 
-table = plt.table(cellText=outFinal, colLabels=['Tipo','Tamanho: 500', 'Tamanho: 1000', 'Tamanho: 2000'], loc='center', 
-                  cellLoc='center', colColours=['#FFFFFF','#FFFFFF', '#F3CC32', '#2769BD'])
+out = minFreeMemory[:1]
+out.append(minFreeMemory[1])
+out.append(minFreeMemory[4])
+out.append(minFreeMemory[7])
+outFinal.append(out)
+
+table = plt.table(cellText=outFinal, colLabels=['Tipo','Tamanho: 500', 'Tamanho: 1000', 'Tamanho: 2000', 'Desempenho'], loc='center', 
+                  cellLoc='center', colColours=['#FFFFFF','#FFFFFF', '#FFFFFF', '#FFFFFF'])
 table.auto_set_font_size(False)
 h = table.get_celld()[(0,0)].get_height()
 w = table.get_celld()[(0,0)].get_width()
 
-plt.title("Caso 1")
+plt.title("Mínimo encontrado em todos os casos.\n Tabela gerada através do result.py.")
 plt.axis('off')
 plt.show()
